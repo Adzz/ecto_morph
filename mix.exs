fadedebcc560f0a868ffc27441aeb3b073c467e3
Adam Lancaster <lancaster28@hotmail.com>
2019-05-19 15:42:07 +0100

    bump version

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule EctoMorph.MixProject do
       app: :ecto_morph,
       licenses: "",
       description: description(),
-      version: "0.1.3",
+      version: "0.1.4",
       package: package(),
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
