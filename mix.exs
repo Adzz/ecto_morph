faded33e11df1094d7a3142226b3897a9f31e7fb
Adz <email@test.example>
2019-07-14 16:12:18 +0100

    bump to 0.1.10

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule EctoMorph.MixProject do
       app: :ecto_morph,
       licenses: "",
       description: description(),
-      version: "0.1.9",
+      version: "0.1.10",
       package: package(),
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
